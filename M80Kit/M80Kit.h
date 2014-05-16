0b0ba288b8d4ebc153b4456cb4a3285fc32122d1
xiangwangfeng <xiangwangfeng@gmail.com>
2014-05-16 10:35:47 +0800

    Initial Commit

---
+++ M80Kit/M80Kit.h
@@ -0,0 +1,13 @@
+//
+//  M80Kit.h
+//  M80Kit
+//
+//  Created by amao on 5/16/14.
+//  Copyright (c) 2014 amao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface M80Kit : NSObject
+
+@end
---
+++ M80Kit/M80Kit.m
@@ -0,0 +1,13 @@
+//
+//  M80Kit.m
+//  M80Kit
+//
+//  Created by amao on 5/16/14.
+//  Copyright (c) 2014 amao. All rights reserved.
+//
+
+#import "M80Kit.h"
+
+@implementation M80Kit
+
+@end
