5fee5c7324ec32eb33a974462cd6804a606c644e
xiangwangfeng <xiangwangfeng@gmail.com>
2014-05-16 15:41:12 +0800

    添加图片的处理方法
    内缩放，外缩放，按照总像素点缩放，保存为JPEG，修正Orientation

--- M80Kit/Classes/Categories/UIImage+M80.h
+++ M80Kit/Classes/Categories/UIImage+M80.h
@@ -8,5 +8,16 @@
 
 
 @interface UIImage (M80)
+//内缩放，一条变等于最长边，另外一条小于等于最长边
+- (UIImage *)m80InternalScaled:(CGSize)newSize;
 
+//采用外缩放：一遍等于请求长度，一遍大于等于请求长度
+- (UIImage *)m80ExternalScaled:(CGSize)newSize;
+
+- (UIImage *)m80ScaledWithPixels:(CGFloat)pixels;
+
+- (BOOL)m80SaveAsJPEG:(NSString *)filepath
+              quality:(CGFloat)quality;
+
+- (UIImage *)m80FixOrientation;
 @end
