f52579bbc0545320505e50729bb41041447177bc
xiangwangfeng <xiangwangfeng@gmail.com>
2014-05-16 16:13:00 +0800

    添加M80TimerHolder
    目的:减少忘记reset NSTimer带来的问题

---
+++ M80Kit/Classes/Util/M80Util.h
@@ -0,0 +1,14 @@
+//
+//  M80Util.h
+//  M80Kit
+//
+//  Created by amao on 5/16/14.
+//  Copyright (c) 2014 amao. All rights reserved.
+//
+
+#ifndef M80Kit_M80Util_h
+#define M80Kit_M80Util_h
+
+#import "M80TimerHolder.h"
+
+#endif
