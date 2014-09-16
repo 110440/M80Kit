bdf12ddc326184ca913c2b82b2fd2b76528d2d55
xiangwangfeng <xiangwangfeng@gmail.com>
2014-09-16 18:37:29 +0800

    UIColor Macro

--- M80Kit/Classes/M80Macro.h
+++ M80Kit/Classes/M80Macro.h
@@ -8,3 +8,18 @@
 
 #import <Foundation/Foundation.h>
 
+#pragma mark - UIColor 
+
+#define M80RGB(rgbValue) [UIColor \
+colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 \
+green:((float)((rgbValue & 0x00FF00) >> 8))/255.0 \
+blue:((float)(rgbValue & 0x0000FF))/255.0 \
+alpha:1.0]
+
+
+#define M80RGBA(rgbaValue) [UIColor \
+colorWithRed:((float)((rgbValue & 0xFF000000) >> 24))/255.0 \
+green:((float)((rgbValue & 0x00FF0000) >> 16))/255.0 \
+blue:((float)(rgbValue & 0x0000FF00) >> 8)/255.0 \
+alpha:((float)(rgbValue & 0x000000FF))/255.0]
+
