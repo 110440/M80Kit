9a0b79507eb7a60b10290708bf3f7df1e90317ed
xiangwangfeng <xiangwangfeng@gmail.com>
2015-03-16 14:05:07 +0800

    misc

--- M80Kit/Classes/Categories/NSData+M80.h
+++ M80Kit/Classes/Categories/NSData+M80.h
@@ -14,21 +14,3 @@
 - (NSString *)m80MD5;
 @end
 
-
-@interface NSData  (M80Crypto)
-- (NSData *)m80AES128Encrypt:(NSString *)key;
-- (NSData *)m80AES192Encrypt:(NSString *)key;
-- (NSData *)m80AES256Encrypt:(NSString *)key;
-- (NSData *)m80DESEncrypt:(NSString *)key;
-- (NSData *)m803DESEncrypt:(NSString *)key;
-- (NSData *)m80RC4Encrypt:(NSString *)key;
-
-
-
-- (NSData *)m80AES128Decrypt:(NSString *)key;
-- (NSData *)m80AES192Decrypt:(NSString *)key;
-- (NSData *)m80AES256Decrypt:(NSString *)key;
-- (NSData *)m80DESDecrypt:(NSString *)key;
-- (NSData *)m803DESDecrypt:(NSString *)key;
-- (NSData *)m80RC4Decrypt:(NSString *)key;
-@end
